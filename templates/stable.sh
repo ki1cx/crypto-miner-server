082cc92a20ded0f75ceddd26b1a74fe9319a6c97
Ki Chang <kiwchang@gmail.com>
2018-02-28 14:52:07 -0800

    set default fan speed to 50%

--- templates/stable.sh
+++ templates/stable.sh
@@ -15,7 +15,7 @@ nvidia-settings -c :0 -a GPUGraphicsClockOffset[3]=0
 nvidia-settings -c :0 -a GPUMemoryTransferRateOffset[3]=1300
 
 ## control fan speed across all GPUs
-#nvidia-settings -c :0 -a GPUFanControlState=1 -a GPUTargetFanSpeed=50
+nvidia-settings -c :0 -a GPUFanControlState=1 -a GPUTargetFanSpeed=50
 
 ## control fan speed for specific GPU
 #nvidia-settings -c :0 -a [gpu:0]/GPUFanControlState=1 -a [fan:0]/GPUTargetFanSpeed=100
